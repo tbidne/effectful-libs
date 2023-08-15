8d92b25418f8a6bf3d37838bb59ff91286371599
Tommy Bidne <tbidne@protonmail.com>
2023-08-15 17:33:29 +1200

    Add FlexibleContexts to cabal files

--- effectful-env/src/Effectful/System/Environment.hs
+++ effectful-env/src/Effectful/System/Environment.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE FlexibleContexts #-}
 
 {- ORMOLU_DISABLE -}
 
--- effectful-unix-compat/src/Effectful/System/PosixCompat.hs
+++ effectful-unix-compat/src/Effectful/System/PosixCompat.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE FlexibleContexts #-}
 
 -- | Provides the 'PosixCompat' effect.
 --
