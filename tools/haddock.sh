f1c7d004494844508fc0ff4769a71b6b821e8910
Tommy Bidne <tbidne@protonmail.com>
2023-08-17 16:52:18 +1200

    Add haddock tool

---
+++ tools/haddock.sh
@@ -0,0 +1,20 @@
+set -e
+
+export LANG="C.UTF-8"
+
+export dirs="*-effectful"
+
+# shellcheck disable=SC2086
+cabal haddock all --haddock-hyperlink-source --haddock-quickjump
+
+mkdir -p docs/
+
+# shellcheck disable=SC2038
+find docs/* ! -path docs/index.html ! -path docs/linuwial.css | xargs -I % sh -c "rm -r %"
+
+for d in $dirs; do
+  # shellcheck disable=SC2086
+  rm -rf docs/$d*
+  # shellcheck disable=SC2086
+  cp -r dist-newstyle/build/x86_64-linux/ghc-9.4.5/$d-0.1/doc/html/* docs/
+done
