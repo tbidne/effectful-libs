7655d647ac2405959a5566f85d48c6529e0b56ef
Tommy Bidne <tbidne@protonmail.com>
2023-09-18 21:29:12 +1200

    Improve optparse utils

--- lib/optparse-effectful/src/Effectful/Optparse/Utils.hs
+++ lib/optparse-effectful/src/Effectful/Optparse/Utils.hs
@@ -8,8 +8,8 @@ module Effectful.Optparse.Utils
   )
 where
 
-import Control.Exception (Exception (displayException))
-import Effectful.FileSystem.Utils (OsPath, encodeFpToOs, encodeFpToValidOs)
+import Effectful.FileSystem.Utils (OsPath)
+import Effectful.FileSystem.Utils qualified as FsUtils
 import Options.Applicative (ReadM)
 import Options.Applicative qualified as OA
 
@@ -17,19 +17,11 @@ import Options.Applicative qualified as OA
 --
 -- @since 0.1
 osPath :: ReadM OsPath
-osPath = do
-  pathStr <- OA.str
-  case encodeFpToOs pathStr of
-    Right p -> pure p
-    Left ex -> fail $ "Error encoding string path: " ++ displayException ex
+osPath = OA.str >>= FsUtils.encodeFpToOsFail
 
 -- | 'OsPath' 'OA.Option' reader. This includes validation i.e. fails if the
 -- path is considered invalid on the given platform.
 --
 -- @since 0.1
 validOsPath :: ReadM OsPath
-validOsPath = do
-  pathStr <- OA.str
-  case encodeFpToValidOs pathStr of
-    Right p -> pure p
-    Left ex -> fail $ "Error encoding string path: " ++ displayException ex
+validOsPath = OA.str >>= FsUtils.encodeFpToValidOsFail
