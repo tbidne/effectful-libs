0fdca33426283212b53cc6f6ce7cfaac8f62804a
Tommy Bidne <tbidne@protonmail.com>
2024-07-06 12:25:01 +1200

    Update bounds, nix, ci

--- flake.nix
+++ flake.nix
@@ -20,43 +20,48 @@
     inputs.nix-hs-utils.follows = "nix-hs-utils";
   };
   outputs =
-    inputs@{ flake-parts
-    , nix-hs-utils
-    , self
-    , ...
+    inputs@{
+      flake-parts,
+      nix-hs-utils,
+      self,
+      ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      perSystem = { pkgs, ... }:
+      perSystem =
+        { pkgs, ... }:
         let
-          ghc-version = "ghc963";
+          ghc-version = "ghc982";
           hlib = pkgs.haskell.lib;
           compiler = pkgs.haskell.packages."${ghc-version}".override {
-            overrides = final: prev: {
-              hedgehog = prev.hedgehog_1_4;
-              hlint = prev.hlint_3_6_1;
-              ormolu = prev.ormolu_0_7_2_0;
-              tasty-hedgehog = prev.tasty-hedgehog_1_4_0_2;
-            } // nix-hs-utils.mkLibs inputs final [
-              "algebra-simple"
-              "bounds"
-            ];
+            overrides =
+              final: prev:
+              { }
+              // nix-hs-utils.mkLibs inputs final [
+                "algebra-simple"
+                "bounds"
+              ];
           };
-          pkgsCompiler = { inherit pkgs compiler; };
-          hsOverlay =
-            (compiler.extend (hlib.compose.packageSourceOverrides {
-              environment-effectful = ./lib/environment-effectful;
-              exceptions-effectful = ./lib/exceptions-effectful;
-              fs-effectful = ./lib/fs-effectful;
-              ioref-effectful = ./lib/ioref-effectful;
-              logger-effectful = ./lib/logger-effectful;
-              logger-ns-effectful = ./lib/logger-ns-effectful;
-              optparse-effectful = ./lib/optparse-effectful;
-              stm-effectful = ./lib/stm-effectful;
-              terminal-effectful = ./lib/terminal-effectful;
-              concurrent-effectful = ./lib/concurrent-effectful;
-              time-effectful = ./lib/time-effectful;
-              unix-compat-effectful = ./lib/unix-compat-effectful;
-            }));
+          pkgsCompiler = {
+            inherit pkgs compiler;
+          };
+          hsOverlay = (
+            compiler.extend (
+              hlib.compose.packageSourceOverrides {
+                environment-effectful = ./lib/environment-effectful;
+                exceptions-effectful = ./lib/exceptions-effectful;
+                fs-effectful = ./lib/fs-effectful;
+                ioref-effectful = ./lib/ioref-effectful;
+                logger-effectful = ./lib/logger-effectful;
+                logger-ns-effectful = ./lib/logger-ns-effectful;
+                optparse-effectful = ./lib/optparse-effectful;
+                stm-effectful = ./lib/stm-effectful;
+                terminal-effectful = ./lib/terminal-effectful;
+                concurrent-effectful = ./lib/concurrent-effectful;
+                time-effectful = ./lib/time-effectful;
+                unix-compat-effectful = ./lib/unix-compat-effectful;
+              }
+            )
+          );
           packages = p: [
             p.environment-effectful
             p.exceptions-effectful
@@ -72,53 +77,50 @@
             p.unix-compat-effectful
           ];
 
-          mkPkg = name: root: source-overrides: compiler.developPackage {
-            inherit name root source-overrides;
-            returnShellEnv = false;
+          mkPkg =
+            name: root: source-overrides:
+            compiler.developPackage {
+              inherit name root source-overrides;
+              returnShellEnv = false;
+            };
+          mkPkgsExceptions =
+            name: root: mkPkg name root { exceptions-effectful = ./lib/exceptions-effectful; };
+          compilerPkgs = {
+            inherit compiler pkgs;
           };
-          mkPkgsExceptions = name: root: mkPkg name root {
-            exceptions-effectful = ./lib/exceptions-effectful;
-          };
-          compilerPkgs = { inherit compiler pkgs; };
         in
         {
           packages.concurrent-effectful = mkPkgsExceptions "concurrent-effectful" ./lib/concurrent-effectful;
           packages.env-guard-effectful = mkPkg "env-guard-effectful" ./lib/env-guard-effectful { };
           packages.environment-effectful = mkPkg "environment-effectful" ./lib/environment-effectful { };
           packages.exceptions-effectful = mkPkg "exceptions-effectful" ./lib/exceptions-effectful { };
-          packages.fs-effectful =
-            mkPkg "fs-effectful" ./lib/fs-effectful {
-              exceptions-effectful = ./lib/exceptions-effectful;
-              ioref-effectful = ./lib/ioref-effectful;
-              unix-compat-effectful = ./lib/unix-compat-effectful;
-            };
+          packages.fs-effectful = mkPkg "fs-effectful" ./lib/fs-effectful {
+            exceptions-effectful = ./lib/exceptions-effectful;
+            ioref-effectful = ./lib/ioref-effectful;
+            unix-compat-effectful = ./lib/unix-compat-effectful;
+          };
           packages.ioref-effectful = mkPkgsExceptions "ioref-effectful" ./lib/ioref-effectful;
           packages.logger-effectful = mkPkgsExceptions "logger-effectful" ./lib/logger-effectful;
-          packages.logger-ns-effectful =
-            mkPkg "logger-ns-effectful" ./lib/logger-ns-effectful {
-              exceptions-effectful = ./lib/exceptions-effectful;
-              logger-effectful = ./lib/logger-effectful;
-              time-effectful = ./lib/time-effectful;
-            };
-          packages.optparse-effectful =
-            mkPkg "optparse-effectful" ./lib/optparse-effectful {
-              exceptions-effectful = ./lib/exceptions-effectful;
-              fs-effectful = ./lib/fs-effectful;
-              ioref-effectful = ./lib/ioref-effectful;
-              unix-compat-effectful = ./lib/unix-compat-effectful;
-            };
+          packages.logger-ns-effectful = mkPkg "logger-ns-effectful" ./lib/logger-ns-effectful {
+            exceptions-effectful = ./lib/exceptions-effectful;
+            logger-effectful = ./lib/logger-effectful;
+            time-effectful = ./lib/time-effectful;
+          };
+          packages.optparse-effectful = mkPkg "optparse-effectful" ./lib/optparse-effectful {
+            exceptions-effectful = ./lib/exceptions-effectful;
+            fs-effectful = ./lib/fs-effectful;
+            ioref-effectful = ./lib/ioref-effectful;
+            unix-compat-effectful = ./lib/unix-compat-effectful;
+          };
           packages.stm-effectful = mkPkgsExceptions "stm-effectful" ./lib/stm-effectful;
           packages.terminal-effectful = mkPkgsExceptions "terminal-effectful" ./lib/terminal-effectful;
           packages.time-effectful = mkPkgsExceptions "time-effectful" ./lib/time-effectful;
-          packages.unix-compat-effectful =
-            mkPkgsExceptions "unix-compat-effectful" ./lib/unix-compat-effectful;
+          packages.unix-compat-effectful = mkPkgsExceptions "unix-compat-effectful" ./lib/unix-compat-effectful;
 
           devShells.default = hsOverlay.shellFor {
             inherit packages;
             withHoogle = true;
-            buildInputs =
-              (nix-hs-utils.mkBuildTools pkgsCompiler)
-              ++ (nix-hs-utils.mkDevTools pkgsCompiler);
+            buildInputs = (nix-hs-utils.mkBuildTools pkgsCompiler) ++ (nix-hs-utils.mkDevTools pkgsCompiler);
           };
 
           apps = {
@@ -128,6 +130,8 @@
           };
         };
       systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
         "x86_64-darwin"
         "x86_64-linux"
       ];
