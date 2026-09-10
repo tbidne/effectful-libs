b042545ad486e9fdcdb739e07188601d713b11c8
Tommy Bidne <tbidne@gmail.com>
2026-09-11 08:51:43 +1200

    Update deps

--- tools/outdated.sh
+++ tools/outdated.sh
@@ -6,15 +6,17 @@ export dirs="lib/effectful-utils lib/*-effectful"
 
 any_outdated=0
 for d in $dirs; do
-  echo "$d"
   cd "$d"
-  out=$(cabal outdated)
-  echo $out
-  echo ""
+  # err to /dev/null to trim extraneous logs
+  out=$(cabal outdated 2> /dev/null)
   cd ../../
 
   if [[ $out != "All dependencies are up to date." ]]; then
     any_outdated=1
+    echo -e "\n*** $d ***"
+    echo -e "$out\n"
+  else
+    echo "*** $d ***"
   fi
 done
 
